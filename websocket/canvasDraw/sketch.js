2ab0529d0a81e8eca70499db6529952bd460f358
Risingvirtue <risingvirtue@berkeley.edu>
2017-12-17 21:45:18 -0800

    erase

--- websocket/canvasDraw/sketch.js
+++ websocket/canvasDraw/sketch.js
@@ -5,30 +5,63 @@ var mouseDown =  false;
 var mousePos = {};
 var preMousePos = {x: -1, y:-1};
 var r = 20;
-var color = "white";
+var color = "blue";
+var rect = canvas.getBoundingClientRect();
+var noDraw = false;
+var erase = false;
 $(document).ready(function() {
-	ctx.fillRect(0,0, canvas.width, canvas.height);
+	
 	socket = io.connect('http://localhost:3000');
 	socket.on('mouse', otherDraw);
+	$("#pencil").toggleClass("select");
+	
+	$("#pencil").click(function() {
+		if (erase) {
+			erase = false;
+			$("#pencil").toggleClass("select");
+			$("#eraser").toggleClass("select");
+		}
+	});
+	
+	$("#eraser").click(function() {
+		if (!erase) {
+			erase = true;
+			$("#pencil").toggleClass("select");
+			$("#eraser").toggleClass("select");
+		}
+	});
 	
 });
 
+
+
 function otherDraw(data) {
+	if (data.erase) {
+		ctx.beginPath();
+		ctx.strokeStyle = "white";
 	
-	ctx.beginPath();
-	ctx.strokeStyle = 'red';
+		ctx.moveTo(data.x1, data.y1);
+		ctx.lineCap = 'round';
+		ctx.lineWidth = 40;
+		ctx.lineTo(data.x2, data.y2);
+		ctx.stroke();
+		ctx.closePath();
+	} else {
+		ctx.beginPath();
+		ctx.strokeStyle = data.color;
+		ctx.moveTo(data.x1, data.y1);
+		ctx.lineCap = 'round';
+		ctx.lineWidth = 10;
+		ctx.lineTo(data.x2, data.y2);
+		ctx.stroke();
+		ctx.closePath();
+	}
 	
-	ctx.moveTo(data.x1, data.y1);
-	ctx.lineCap = 'round';
-	ctx.lineWidth = 10;
-	ctx.lineTo(data.x2, data.y2);
-	ctx.stroke();
-	ctx.closePath();
 }
 document.body.addEventListener("mousedown", function (e) {
 	mouseDown = true;
-	mousePos.x = e.pageX;
-	mousePos.y = e.pageY;
+	mousePos.x = e.pageX - rect.left;
+	mousePos.y = e.pageY - rect.top;
 	
 });
 
@@ -38,19 +71,37 @@ document.body.addEventListener("mouseup", function (e) {
 });
 
 document.body.addEventListener("mousemove", function (e) {
+	if (noDraw) {
+		return;
+	}
+	
 	//console.log(mousePos);
-	if (mouseDown) {
-		var data = {x1: mousePos.x, y1: mousePos.y, x2: e.x, y2: e.y};
+	eX = e.pageX - rect.left;
+	eY = e.pageY - rect.top;
+	if (mouseDown && !erase) {
+		var data = {x1: mousePos.x, y1: mousePos.y, x2: eX, y2: eY, color: color, erase: false};
 		socket.emit('mouse', data);
 		ctx.beginPath();
 		ctx.strokeStyle = color;
 		ctx.moveTo(mousePos.x, mousePos.y);
 		ctx.lineCap = 'round';
 		ctx.lineWidth = 10;
-		ctx.lineTo(e.pageX, e.pageY);
+		ctx.lineTo(eX, eY);
 		ctx.stroke();
 		ctx.closePath();
-		mousePos.x = e.pageX; mousePos.y = e.pageY;
+		mousePos.x = eX; mousePos.y = eY;
+	} else if (mouseDown && erase) {
+		var data = {x1: mousePos.x, y1: mousePos.y, x2: eX, y2: eY, color: color, erase: true};
+		socket.emit('mouse', data);
+		ctx.beginPath();
+		ctx.strokeStyle = "white";
+		ctx.moveTo(mousePos.x, mousePos.y);
+		ctx.lineCap = 'round';
+		ctx.lineWidth = 40;
+		ctx.lineTo(eX, eY);
+		ctx.stroke();
+		ctx.closePath();
+		mousePos.x = eX; mousePos.y = eY;
 	}
 	
 	
